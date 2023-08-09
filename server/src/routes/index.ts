87124b51ac821af7d64a32f773fdfa73425ef8eb
Bruno Sobral - siscon <brunosobralss@hotmail.com>
2023-08-09 10:46:40 -0300

    added author in package.json

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -5,7 +5,7 @@ const router: Router = express.Router();
 router.get('/api', (req: Request, res: Response) => {
   res.status(200).send({
     success: 'true',
-    message: 'Seja bem-vindo(a) a API Node.js + PostgreSQL + Azure!',
+    message: 'Welcome to API Node.js, Typescript, PostgreSQL!',
     version: '1.0.0',
   });
 });
